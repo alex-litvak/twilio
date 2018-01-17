6f15b2d82cbf1ccb2fee8475a9dcfc0dd7b4ac8e
Alex <alex.litvak@iron.io>
2018-01-17 18:20:50 +0600

    Add worker files

---
+++ sms.rb
@@ -0,0 +1,24 @@
+require_relative 'bundle/bundler/setup'
+require 'twilio-ruby'
+require 'iron_worker'
+
+sid = IronWorker.payload["sid"]
+token = IronWorker.payload["token"]
+from = IronWorker.payload["from"]
+to = IronWorker.payload["to"]
+body = IronWorker.payload["body"]
+# Info on Twilio API params: https://www.twilio.com/docs/api/rest/making-calls
+
+# set up a client to talk to the Twilio REST API
+@client = Twilio::REST::Client.new sid, token
+
+puts "Sending sms..."
+r = @client.messages.create(
+    :from => from,
+    :to => to,
+    :body => body
+)
+
+puts "Twilio response:"
+p r
+
